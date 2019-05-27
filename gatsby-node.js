efbbeb7ce75aa075b959a41b77069d3afa4818fe
Edison Ade <ask@buzzedison.com>
2019-05-27 17:20:42 +0100

    test

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
-
+ */
 
 // You can delete this file if you're not using it
 
@@ -28,7 +28,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
      * during the build. (See `src/utils/auth.js` to see how we prevent this
      * from breaking the app.)
      */
-/** actions.setWebpackConfig({
+    actions.setWebpackConfig({
       module: {
         rules: [
           {
@@ -40,9 +40,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     })
   }
 }
-
-*/
-/** const slash = require("slash")
+const slash = require("slash")
 const path = require("path")
 
 exports.createPages = ({ graphql, actions }) => {
@@ -81,6 +79,3 @@ exports.createPages = ({ graphql, actions }) => {
       console.log("Error retrieving contentful data", error)
     })
 }
-
-//testinggg
-*/
